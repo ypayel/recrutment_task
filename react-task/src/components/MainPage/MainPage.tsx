8c7480b08c43369ed460c986e6478e61c9c77175
ypayel <kukankovartem@gmail.com>
2024-07-08 23:30:46 +0200

    update
    more styles

--- react-task/src/components/MainPage/MainPage.tsx
+++ react-task/src/components/MainPage/MainPage.tsx
@@ -2,17 +2,19 @@ import "./MainPage.scss"
 export const MainPage = () => {
     return (
         <>
+         <div className="main-container">
         <div className="header-conteiner">
-        <h1 className="header">Демо-версия</h1>
+        <h2 className="header">Демо-версия</h2>
         <p className="text">Lorem ipsum dolor,
           sit amet consectetur adipisicing elit.
           Impedit sit ut exercitationem accusantium non!
           Ducimus ratione quos ad quasi esse dolore aperiam?
           Illum nobis suscipit quis inventore earum velit explicabo?
           </p>
-          <button className='click-button'>Попробовать бесплатно</button>
+          <button className='click-button'>Попробовать бесплатно <img src="/images/up-arrow.png" alt="arrow" className="icon-arrow" /></button>
       </div>
       <img src="/images/comp.svg" alt="computer" className='computer-image' />
+      </div>
         </>
     )
     
